07d378b0a5f03541b5c4cfcdbdc7b924637485cd
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2019-08-27 08:25:50 +0200

    chore: Set up linting with Standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,18 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: [
-    'ember',
+    'ember'
   ],
   extends: [
-    'eslint:recommended',
+    'standard',
     'plugin:ember/recommended',
-    '@bagaar/eslint-config',
-    '@bagaar/eslint-config-ember',
+    '@bagaar/eslint-config-ember'
   ],
   env: {
-    browser: true,
+    browser: true
   },
   rules: {},
   overrides: [
@@ -28,25 +27,25 @@ module.exports = {
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'tests/dummy/config/**/*.js',
+        'tests/dummy/config/**/*.js'
       ],
       excludedFiles: [
         'addon/**',
         'addon-test-support/**',
         'app/**',
-        'tests/dummy/app/**',
+        'tests/dummy/app/**'
       ],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: 'script'
       },
       env: {
         browser: false,
-        node: true,
+        node: true
       },
       plugins: ['node'],
       rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
         // add your custom rules and overrides for node files here
-      }),
-    },
-  ],
-};
+      })
+    }
+  ]
+}
