29a127f4ff034d4a507eca13d4a76f7d8d94f28c
Bert De Block <bert.deblock@bagaar.be>
2023-07-06 12:22:41 +0200

    chore!: drop support for Ember versions below v4.4

--- README.md
+++ README.md
@@ -18,8 +18,8 @@ Easy customisable pagination component for Ember applications.
 
 ## Compatibility
 
-- Ember.js v3.24 or above
-- Ember CLI v3.24 or above
+- Ember.js v4.4 or above
+- Ember CLI v4.4 or above
 - Node.js v16 or above
 
 ## Installation
--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -8,22 +8,6 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.28',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
